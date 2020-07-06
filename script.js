2094a469d640b8af3a1aae79b76a552c2577a1c1
CodeOne45 <malikaman1999@gmail.com>
2020-07-06 20:27:16 +0200

    Calcul v2.0

--- script.js
+++ script.js
@@ -83,7 +83,11 @@ class Calculator {
       this.currentOperand
     );
     if (this.operation != null) {
-      this.previousOperandTextElement.innerText = ` ${this.previousOperand}  ${this.operation} `;
+      this.previousOperandTextElement.innerText = `${this.getDisplayNumber(
+        this.previousOperand
+      )} ${this.operation}`;
+    } else {
+      this.previousOperandTextElement.innerText = "";
     }
   }
 }
